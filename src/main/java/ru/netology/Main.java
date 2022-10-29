5b345e012b8224265c7b241ac5ee83e0827693e7
Max Shvagerus <mshvagerus@gmail.com>
2022-10-29 18:03:19 +0300

    done

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,6 +2,17 @@ package ru.netology;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "1234 № 98765432";
+        post.patronymic = "Федорович";
+        post.phone = "+7 (905)-999-01-23";
+        post.surname = "Крузенштерн";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
     }
 }
